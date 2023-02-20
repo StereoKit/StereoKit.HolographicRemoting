16bb7f6fdf3792ef37a83a7b562959594759a2f7
Edralos <32432267+Edralos@users.noreply.github.com>
2023-02-20 19:47:50 +0100

    spaces -> tabs

--- StereoKit.HolographicRemoting/HolographicRemoting.cs
+++ StereoKit.HolographicRemoting/HolographicRemoting.cs
@@ -5,88 +5,88 @@ using System.Runtime.InteropServices;
 
 namespace StereoKit.HolographicRemoting
 {
-    public class HolographicRemoting : IStepper
-    {
-        static readonly string remotingExtName = "XR_MSFT_holographic_remoting";
+	public class HolographicRemoting : IStepper
+	{
+		static readonly string remotingExtName = "XR_MSFT_holographic_remoting";
 
-        private string _ipAddress;
-        private ushort _port;
-        private bool   _sendAudio;
-        private int    _maxBitrate;
-        private bool   _wideAudioCapture;
+		private string	_ipAddress;
+		private ushort	_port;
+		private bool	_sendAudio;
+		private int		_maxBitrate;
+		private bool	_wideAudioCapture;
 
-        public  bool Enabled { get; private set; }
+		public  bool Enabled { get; private set; }
 
-        public HolographicRemoting(string ipAddress, ushort port = 8265, bool sendAudio = true, int maxBitrateKbps = 20000, bool wideAudioCapture = true)
-        {
-            _ipAddress          = ipAddress;
-            _port               = port;
-            _sendAudio          = sendAudio;
-            _maxBitrate         = maxBitrateKbps;
-            _wideAudioCapture   = wideAudioCapture;
+		public HolographicRemoting(string ipAddress, ushort port = 8265, bool sendAudio = true, int maxBitrateKbps = 20000, bool wideAudioCapture = true)
+		{
+			_ipAddress			= ipAddress;
+			_port				= port;
+			_sendAudio			= sendAudio;
+			_maxBitrate			= maxBitrateKbps;
+			_wideAudioCapture	= wideAudioCapture;
 
-            if (SK.IsInitialized)
-                Log.Err("HolographicRemoting must be constructed before StereoKit is initialized!");
+			if (SK.IsInitialized)
+				Log.Err("HolographicRemoting must be constructed before StereoKit is initialized!");
 
-            Backend.OpenXR.RequestExt(remotingExtName);
-            Backend.OpenXR.OnPreCreateSession += OnPreCreateSession;
+			Backend.OpenXR.RequestExt(remotingExtName);
+			Backend.OpenXR.OnPreCreateSession += OnPreCreateSession;
 
-            // Set up the OpenXR manifest for the remoting runtime!
-            string runtimePath = Path.GetDirectoryName(typeof(HolographicRemoting).Assembly.Location);
-            runtimePath = Path.Combine(runtimePath, "RemotingXR.json");
-            Environment.SetEnvironmentVariable("XR_RUNTIME_JSON", runtimePath);
-        }
+			// Set up the OpenXR manifest for the remoting runtime!
+			string runtimePath = Path.GetDirectoryName(typeof(HolographicRemoting).Assembly.Location);
+			runtimePath = Path.Combine(runtimePath, "RemotingXR.json");
+			Environment.SetEnvironmentVariable("XR_RUNTIME_JSON", runtimePath);
+		}
 
-        public bool Initialize() => Enabled;
-        public void Shutdown  () { }
-        public void Step      () { }
+		public bool Initialize	() => Enabled;
+		public void Shutdown	() { }
+		public void Step		() { }
 
-        void OnPreCreateSession()
-        {
-            Enabled = Backend.OpenXR.ExtEnabled(remotingExtName);
-            if (!Enabled) return;
+		void OnPreCreateSession()
+		{
+			Enabled = Backend.OpenXR.ExtEnabled(remotingExtName);
+			if (!Enabled) return;
 
-            NativeAPI.LoadFunctions();
+			NativeAPI.LoadFunctions();
 
-            Log.Info($"Connecting to Holographic Remoting Player at {_ipAddress} : {_port} ...");
+			Log.Info($"Connecting to Holographic Remoting Player at {_ipAddress} : {_port} ...");
 
-            XrRemotingRemoteContextPropertiesMSFT contextProperties = new XrRemotingRemoteContextPropertiesMSFT();
-            contextProperties.type                        = XrStructureType.REMOTING_REMOTE_CONTEXT_PROPERTIES_MSFT;
-            contextProperties.enableAudio                 = _sendAudio ? 1 : 0;
-            contextProperties.maxBitrateKbps              = (uint)_maxBitrate;
-            contextProperties.videoCodec                  = XrRemotingVideoCodecMSFT.H265;
-            contextProperties.depthBufferStreamResolution = XrRemotingDepthBufferStreamResolutionMSFT.HALF;
+			XrRemotingRemoteContextPropertiesMSFT contextProperties = new XrRemotingRemoteContextPropertiesMSFT();
+			contextProperties.type							= XrStructureType.REMOTING_REMOTE_CONTEXT_PROPERTIES_MSFT;
+			contextProperties.enableAudio					= _sendAudio ? 1 : 0;
+			contextProperties.maxBitrateKbps				= (uint)_maxBitrate;
+			contextProperties.videoCodec					= XrRemotingVideoCodecMSFT.H265;
+			contextProperties.depthBufferStreamResolution	= XrRemotingDepthBufferStreamResolutionMSFT.HALF;
 
-            if (_sendAudio && !_wideAudioCapture)
-            {
-                XrRemotingAudioOutputCaptureSettingsMSFT audioCaptureSettings = new XrRemotingAudioOutputCaptureSettingsMSFT();
-                audioCaptureSettings.type                   = XrStructureType.REMOTING_AUDIO_OUTPUT_CAPTURE_SETTINGS_MSFT;
-                audioCaptureSettings.audioOutputCaptureMode = XrRemotingAudioOutputCaptureModeMSFT.AUDIO_OUTPUT_CAPTURE_MODE_APP_ONLY_MSFT;
-                
-                int     size    = Marshal.SizeOf(typeof(XrRemotingAudioOutputCaptureSettingsMSFT));
-                IntPtr  memory  = Marshal.AllocHGlobal(size);
-                Marshal.StructureToPtr(audioCaptureSettings, memory, false);
+			if (_sendAudio && !_wideAudioCapture)
+			{
+				XrRemotingAudioOutputCaptureSettingsMSFT audioCaptureSettings = new XrRemotingAudioOutputCaptureSettingsMSFT();
+				audioCaptureSettings.type					= XrStructureType.REMOTING_AUDIO_OUTPUT_CAPTURE_SETTINGS_MSFT;
+				audioCaptureSettings.audioOutputCaptureMode = XrRemotingAudioOutputCaptureModeMSFT.AUDIO_OUTPUT_CAPTURE_MODE_APP_ONLY_MSFT;
+				
+				int		size	= Marshal.SizeOf(typeof(XrRemotingAudioOutputCaptureSettingsMSFT));
+				IntPtr	memory	= Marshal.AllocHGlobal(size);
+				Marshal.StructureToPtr(audioCaptureSettings, memory, false);
 
-                contextProperties.next = memory;
-                Marshal.FreeHGlobal(memory);
-            }
+				contextProperties.next = memory;
+				Marshal.FreeHGlobal(memory);
+			}
 
-            if (NativeAPI.xrRemotingSetContextPropertiesMSFT(Backend.OpenXR.Instance, Backend.OpenXR.SystemId, contextProperties) != XrResult.Success)
-            {
-                Log.Warn("xrRemotingSetContextPropertiesMSFT failed!");
-            }
+			if (NativeAPI.xrRemotingSetContextPropertiesMSFT(Backend.OpenXR.Instance, Backend.OpenXR.SystemId, contextProperties) != XrResult.Success)
+			{
+				Log.Warn("xrRemotingSetContextPropertiesMSFT failed!");
+			}
 
-            XrRemotingConnectInfoMSFT connectInfo = new XrRemotingConnectInfoMSFT();
-            connectInfo.type             = XrStructureType.REMOTING_CONNECT_INFO_MSFT;
-            connectInfo.remoteHostName   = Marshal.StringToHGlobalAnsi(_ipAddress);
-            connectInfo.remotePort       = _port;
-            connectInfo.secureConnection = 0;
-            XrResult result = NativeAPI.xrRemotingConnectMSFT(Backend.OpenXR.Instance, Backend.OpenXR.SystemId, connectInfo);
-            if (result != XrResult.Success)
-            {
-                Log.Warn("xrRemotingConnectMSFT failed! " + result);
-            }
-            Marshal.FreeHGlobal(connectInfo.remoteHostName);
-        }
-    }
+			XrRemotingConnectInfoMSFT connectInfo = new XrRemotingConnectInfoMSFT();
+			connectInfo.type				= XrStructureType.REMOTING_CONNECT_INFO_MSFT;
+			connectInfo.remoteHostName		= Marshal.StringToHGlobalAnsi(_ipAddress);
+			connectInfo.remotePort			= _port;
+			connectInfo.secureConnection	= 0;
+			XrResult result = NativeAPI.xrRemotingConnectMSFT(Backend.OpenXR.Instance, Backend.OpenXR.SystemId, connectInfo);
+			if (result != XrResult.Success)
+			{
+				Log.Warn("xrRemotingConnectMSFT failed! " + result);
+			}
+			Marshal.FreeHGlobal(connectInfo.remoteHostName);
+		}
+	}
 }
--- StereoKit.HolographicRemoting/NativeTypes.cs
+++ StereoKit.HolographicRemoting/NativeTypes.cs
@@ -3,130 +3,130 @@ using System.Runtime.InteropServices;
 
 namespace StereoKit.HolographicRemoting
 {
-    enum XrStructureType : UInt64
-    {
-        REMOTING_REMOTE_CONTEXT_PROPERTIES_MSFT             = 1000065000,
-        REMOTING_CONNECT_INFO_MSFT                          = 1000065001,
-        REMOTING_LISTEN_INFO_MSFT                           = 1000065002,
-        REMOTING_DISCONNECT_INFO_MSFT                       = 1000065003,
-        REMOTING_EVENT_DATA_LISTENING_MSFT                  = 1000065004,
-        REMOTING_EVENT_DATA_CONNECTED_MSFT                  = 1000065005,
-        REMOTING_EVENT_DATA_DISCONNECTED_MSFT               = 1000065006,
-        REMOTING_AUTHENTICATION_TOKEN_REQUEST_MSFT          = 1000065007,
-        REMOTING_CERTIFICATE_DATA_MSFT                      = 1000065008,
-        REMOTING_CERTIFICATE_VALIDATION_RESULT_MSFT         = 1000065009,
-        REMOTING_SERVER_CERTIFICATE_VALIDATION_MSFT         = 1000065010,
-        REMOTING_AUTHENTICATION_TOKEN_VALIDATION_MSFT       = 1000065011,
-        REMOTING_SERVER_CERTIFICATE_REQUEST_MSFT            = 1000065012,
-        REMOTING_SECURE_CONNECTION_CLIENT_CALLBACKS_MSFT    = 1000065013,
-        REMOTING_SECURE_CONNECTION_SERVER_CALLBACKS_MSFT    = 1000065014,
-        EVENT_DATA_REMOTING_DATA_CHANNEL_CREATED_MSFT       = 1000065015,
-        EVENT_DATA_REMOTING_DATA_CHANNEL_OPENED_MSFT        = 1000065016,
-        EVENT_DATA_REMOTING_DATA_CHANNEL_DATA_RECEIVED_MSFT	= 1000065017,
-        EVENT_DATA_REMOTING_DATA_CHANNEL_CLOSED_MSFT        = 1000065018,
-        REMOTING_DATA_CHANNEL_CREATE_INFO_MSFT              = 1000065019,
-        REMOTING_DATA_CHANNEL_STATE_MSFT                    = 1000065020,
-        REMOTING_DATA_CHANNEL_SEND_DATA_INFO_MSFT           = 1000065021,
-        EVENT_DATA_REMOTING_TIMESTAMP_CONVERSION_READY_MSFT = 1000065022,
-        REMOTING_AUDIO_OUTPUT_CAPTURE_SETTINGS_MSFT         = 1000065023,
-    }
+	enum XrStructureType : UInt64
+	{
+		REMOTING_REMOTE_CONTEXT_PROPERTIES_MSFT				= 1000065000,
+		REMOTING_CONNECT_INFO_MSFT							= 1000065001,
+		REMOTING_LISTEN_INFO_MSFT							= 1000065002,
+		REMOTING_DISCONNECT_INFO_MSFT						= 1000065003,
+		REMOTING_EVENT_DATA_LISTENING_MSFT					= 1000065004,
+		REMOTING_EVENT_DATA_CONNECTED_MSFT					= 1000065005,
+		REMOTING_EVENT_DATA_DISCONNECTED_MSFT				= 1000065006,
+		REMOTING_AUTHENTICATION_TOKEN_REQUEST_MSFT			= 1000065007,
+		REMOTING_CERTIFICATE_DATA_MSFT						= 1000065008,
+		REMOTING_CERTIFICATE_VALIDATION_RESULT_MSFT			= 1000065009,
+		REMOTING_SERVER_CERTIFICATE_VALIDATION_MSFT			= 1000065010,
+		REMOTING_AUTHENTICATION_TOKEN_VALIDATION_MSFT		= 1000065011,
+		REMOTING_SERVER_CERTIFICATE_REQUEST_MSFT			= 1000065012,
+		REMOTING_SECURE_CONNECTION_CLIENT_CALLBACKS_MSFT	= 1000065013,
+		REMOTING_SECURE_CONNECTION_SERVER_CALLBACKS_MSFT	= 1000065014,
+		EVENT_DATA_REMOTING_DATA_CHANNEL_CREATED_MSFT		= 1000065015,
+		EVENT_DATA_REMOTING_DATA_CHANNEL_OPENED_MSFT		= 1000065016,
+		EVENT_DATA_REMOTING_DATA_CHANNEL_DATA_RECEIVED_MSFT	= 1000065017,
+		EVENT_DATA_REMOTING_DATA_CHANNEL_CLOSED_MSFT		= 1000065018,
+		REMOTING_DATA_CHANNEL_CREATE_INFO_MSFT				= 1000065019,
+		REMOTING_DATA_CHANNEL_STATE_MSFT					= 1000065020,
+		REMOTING_DATA_CHANNEL_SEND_DATA_INFO_MSFT			= 1000065021,
+		EVENT_DATA_REMOTING_TIMESTAMP_CONVERSION_READY_MSFT = 1000065022,
+		REMOTING_AUDIO_OUTPUT_CAPTURE_SETTINGS_MSFT			= 1000065023,
+	}
 
-    enum XrResult : Int32
-    {
-        Success = 0,
-    }
+	enum XrResult : Int32
+	{
+		Success = 0,
+	}
 
-    enum XrRemotingVideoCodecMSFT : UInt32
-    {
-        ANY  = 0,
-        H264 = 1,
-        H265 = 2,
-        MAX_ENUM  = 0x7FFFFFFF
-    }
+	enum XrRemotingVideoCodecMSFT : UInt32
+	{
+		ANY  = 0,
+		H264 = 1,
+		H265 = 2,
+		MAX_ENUM  = 0x7FFFFFFF
+	}
 
-    enum XrRemotingDepthBufferStreamResolutionMSFT : UInt32
-    {
-        HALF     = 0,
-        FULL     = 1,
-        QUARTER  = 2,
-        DISABLED = 3,
-        MAX_ENUM = 0x7FFFFFFF
-    }
+	enum XrRemotingDepthBufferStreamResolutionMSFT : UInt32
+	{
+		HALF		= 0,
+		FULL		= 1,
+		QUARTER		= 2,
+		DISABLED	= 3,
+		MAX_ENUM	= 0x7FFFFFFF
+	}
 
-    enum XrRemotingConnectionStateMSFT : UInt32
-    {
-        DISCONNECTED = 0,
-        CONNECTING   = 1,
-        CONNECTED    = 2,
-        MAX_ENUM     = 0x7FFFFFFF
-    }
+	enum XrRemotingConnectionStateMSFT : UInt32
+	{
+		DISCONNECTED	= 0,
+		CONNECTING		= 1,
+		CONNECTED		= 2,
+		MAX_ENUM		= 0x7FFFFFFF
+	}
 
-    enum XrRemotingDisconnectReasonMSFT : UInt32
-    {
-        NONE                             = 0,
-        UNKNOWN                          = 1,
-        NO_SERVER_CERTIFICATE            = 2,
-        HANDSHAKE_PORT_BUSY              = 3,
-        HANDSHAKE_UNREACHABLE            = 4,
-        HANDSHAKE_CONNECTION_FAILED      = 5,
-        AUTHENTICATION_FAILED            = 6,
-        REMOTING_VERSION_MISMATCH        = 7,
-        INCOMPATIBLE_TRANSPORT_PROTOCOLS = 8,
-        HANDSHAKE_FAILED                 = 9,
-        TRANSPORT_PORT_BUSY              = 10,
-        TRANSPORT_UNREACHABLE            = 11,
-        TRANSPORT_CONNECTION_FAILED      = 12,
-        PROTOCOL_VERSION_MISMATCH        = 13,
-        PROTOCOL_ERROR                   = 14,
-        VIDEO_CODEC_NOT_AVAILABLE        = 15,
-        CANCELED                         = 16,
-        CONNECTION_LOST                  = 17,
-        DEVICE_LOST                      = 18,
-        DISCONNECT_REQUEST               = 19,
-        HANDSHAKE_NETWORK_UNREACHABLE    = 20,
-        HANDSHAKE_CONNECTION_REFUSED     = 21,
-        VIDEO_FORMAT_NOT_AVAILABLE       = 22,
-        PEER_DISCONNECT_REQUEST          = 23,
-        PEER_DISCONNECT_TIMEOUT          = 24,
-        SESSION_OPEN_TIMEOUT             = 25,
-        REMOTING_HANDSHAKE_TIMEOUT       = 26,
-        INTERNAL_ERROR                   = 27,
-        HANDSHAKE_PERMISSION_DENIED      = 28,
-        MAX_ENUM                         = 0x7FFFFFFF
-    }
+	enum XrRemotingDisconnectReasonMSFT : UInt32
+	{
+		NONE								= 0,
+		UNKNOWN								= 1,
+		NO_SERVER_CERTIFICATE				= 2,
+		HANDSHAKE_PORT_BUSY					= 3,
+		HANDSHAKE_UNREACHABLE				= 4,
+		HANDSHAKE_CONNECTION_FAILED			= 5,
+		AUTHENTICATION_FAILED				= 6,
+		REMOTING_VERSION_MISMATCH			= 7,
+		INCOMPATIBLE_TRANSPORT_PROTOCOLS	= 8,
+		HANDSHAKE_FAILED					= 9,
+		TRANSPORT_PORT_BUSY					= 10,
+		TRANSPORT_UNREACHABLE				= 11,
+		TRANSPORT_CONNECTION_FAILED			= 12,
+		PROTOCOL_VERSION_MISMATCH			= 13,
+		PROTOCOL_ERROR						= 14,
+		VIDEO_CODEC_NOT_AVAILABLE			= 15,
+		CANCELED							= 16,
+		CONNECTION_LOST						= 17,
+		DEVICE_LOST							= 18,
+		DISCONNECT_REQUEST					= 19,
+		HANDSHAKE_NETWORK_UNREACHABLE		= 20,
+		HANDSHAKE_CONNECTION_REFUSED		= 21,
+		VIDEO_FORMAT_NOT_AVAILABLE			= 22,
+		PEER_DISCONNECT_REQUEST				= 23,
+		PEER_DISCONNECT_TIMEOUT				= 24,
+		SESSION_OPEN_TIMEOUT				= 25,
+		REMOTING_HANDSHAKE_TIMEOUT			= 26,
+		INTERNAL_ERROR						= 27,
+		HANDSHAKE_PERMISSION_DENIED			= 28,
+		MAX_ENUM							= 0x7FFFFFFF
+	}
 
-    enum XrRemotingAudioOutputCaptureModeMSFT : UInt32
-    {
-        AUDIO_OUTPUT_CAPTURE_MODE_SYSTEM_WIDE_MSFT  = 0,
-        AUDIO_OUTPUT_CAPTURE_MODE_APP_ONLY_MSFT     = 1
-    }
+	enum XrRemotingAudioOutputCaptureModeMSFT : UInt32
+	{
+		AUDIO_OUTPUT_CAPTURE_MODE_SYSTEM_WIDE_MSFT	= 0,
+		AUDIO_OUTPUT_CAPTURE_MODE_APP_ONLY_MSFT		= 1
+	}
 
-    [StructLayout(LayoutKind.Sequential)]
-    struct XrRemotingRemoteContextPropertiesMSFT
-    {
-        public XrStructureType                           type;
-        public IntPtr                                    next;
-        public uint                                      maxBitrateKbps;
-        public int                                       enableAudio;
-        public XrRemotingVideoCodecMSFT                  videoCodec;
-        public XrRemotingDepthBufferStreamResolutionMSFT depthBufferStreamResolution;
-    }
+	[StructLayout(LayoutKind.Sequential)]
+	struct XrRemotingRemoteContextPropertiesMSFT
+	{
+		public XrStructureType								type;
+		public IntPtr										next;
+		public uint											maxBitrateKbps;
+		public int											enableAudio;
+		public XrRemotingVideoCodecMSFT						videoCodec;
+		public XrRemotingDepthBufferStreamResolutionMSFT	depthBufferStreamResolution;
+	}
 
-    [StructLayout(LayoutKind.Sequential)]
-    struct XrRemotingConnectInfoMSFT
-    {
-        public XrStructureType type;
-        public IntPtr          next;
-        public IntPtr          remoteHostName;
-        public ushort          remotePort;
-        public int             secureConnection;
-    }
+	[StructLayout(LayoutKind.Sequential)]
+	struct XrRemotingConnectInfoMSFT
+	{
+		public XrStructureType	type;
+		public IntPtr			next;
+		public IntPtr			remoteHostName;
+		public ushort			remotePort;
+		public int				secureConnection;
+	}
 
-    [StructLayout(LayoutKind.Sequential)]
-    struct XrRemotingAudioOutputCaptureSettingsMSFT
-    {
-        public XrStructureType                      type;
-        public IntPtr                               next;
-        public XrRemotingAudioOutputCaptureModeMSFT audioOutputCaptureMode;
-    }
+	[StructLayout(LayoutKind.Sequential)]
+	struct XrRemotingAudioOutputCaptureSettingsMSFT
+	{
+		public XrStructureType						type;
+		public IntPtr								next;
+		public XrRemotingAudioOutputCaptureModeMSFT audioOutputCaptureMode;
+	}
 }
